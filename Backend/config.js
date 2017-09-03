5e3c3d9f5f55a2566632cb682cefd5a1d5e1c6dd
Taidy Marrugo <tj.marrugo10@uniandes.edu.co>
2017-09-03 08:15:06 -0500

    Cambio exports del config

--- Backend/config.js
+++ Backend/config.js
@@ -1,12 +1,9 @@
 module.exports = {
     port: process.env.PORT || 2017,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/viccon'
-}
-//se parametriza con la ruta del computador
-module.exports = Object.freeze({
-    rutaMultimedia: 'C:/Users/Taidy/Videos/multimediaViccon/',
+    db: process.env.MONGODB || 'mongodb://localhost:27017/viccon',
+ 	rutaMultimedia: 'C:/Users/Taidy/Videos/multimediaViccon/',
     nombreCarpetaOriginal: 'original/',
     nombreCarpetaConvertida: 'conversion/',
     nombreCarpetaThumb: 'thumb/',
     extension:'.mp4'
-});
+}
