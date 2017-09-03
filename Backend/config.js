9db97646e791394ca0546d00d9ceb7a88c3ac346
Emilio Alvarado <emilio.alvarado@gmail.com>
2017-09-02 23:57:01 -0500

    Revert "Cambios 1.0 de cron, convertidor y envio notificacion correo"
    This reverts commit 189ac872195185c1443d866885611d0a835eb9ec.

--- Backend/config.js
+++ Backend/config.js
@@ -2,11 +2,3 @@ module.exports = {
     port: process.env.PORT || 2017,
     db: process.env.MONGODB || 'mongodb://localhost:27017/viccon'
 }
-//se parametriza con la ruta del computador
-module.exports = Object.freeze({
-    rutaMultimedia: 'C:/Users/Taidy/Videos/multimediaViccon/',
-    nombreCarpetaOriginal: 'original/',
-    nombreCarpetaConvertida: 'conversion/',
-    nombreCarpetaThumb: 'thumb/',
-    extension:'.mp4'
-});
