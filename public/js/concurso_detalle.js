db96f3c5c58e068780716e73ef4de19f600102b4
makito <makitodev@outlook.com>
2017-09-04 11:29:56 -0500

    Modificacion del llamado del api de consulta de concursos para que el parámetro de la URI sea un variable
    Modificacion del llamado del api de consulta de concursos para que el
    parámetro de la URI sea un variable

--- public/js/concurso_detalle.js
+++ public/js/concurso_detalle.js
@@ -2,7 +2,8 @@
 //esta URI lista el detalle del concurso tanto para la pagina (detalle_concurso_admin-html, que es la que 
 //    el adminsitrador ve internamente con todo el detalle como para la pagina detalleconcursopublico.html que es el 
   //  html que el publico ve)
-var URI = 'http://localhost:2017/api/concurso/59ad050daf317f13883fb046';
+var concursoId = '59ad050daf317f13883fb046'
+var URI = `http://localhost:2017/api/concurso/${concursoId}'`;
 var app = angular.module("plunker", ['ngRoute']);
 app.controller("ConcursoPorIdParaPublico", function($scope, $http) {
         $scope.mydata = [];
@@ -13,13 +14,11 @@ app.controller("ConcursoPorIdParaPublico", function($scope, $http) {
              });
     });
 
-
-   
-
 //esta URI2 lista los videos procesados del concurso que tiene el id quemado
 //se utiliza solo en  la pagina detalleconcursopublico.html que es el 
-  //  html que el publico ve)
-var URI2 = 'http://localhost:2017/api/video/59ad050daf317f13883fb046/Procesado';
+//  html que el publico ve)
+idConcurso = '59ad050daf317f13883fb046'
+var URI2 = `http://localhost:2017/api/video/${idConcurso}/Procesado`;
 
 app.controller("2ConcursoPorIdParaPublico", function($scope, $http) {
         $scope.mydata2 = [];
@@ -34,7 +33,6 @@ app.controller("2ConcursoPorIdParaPublico", function($scope, $http) {
 //esta URI 3 es la lista de todos los videos de esa categoria con o sin proceso pues el adminsitrador
 //del concurso los puede ver
 var URI3 = 'http://localhost:2017/api/video/';
-
 app.controller("3ConcursoPorIdParaPublico", function($scope, $http) {
         $scope.mydata3 = [];
         $http.get(URI3)
