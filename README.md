667a38917c648fba6518d7f7746751454f47e36e
Taidy Marrugo <tj.marrugo10@uniandes.edu.co>
2017-09-24 19:35:59 -0500

    Despliege A mongocompleto parte 2

---
+++ README.md
@@ -0,0 +1,19 @@
+# viccon
+video cloud contest
+
+
+## Install environment
+```
+$ npm install
+```
+
+## Run app
+```
+$ hotnode main
+```
+or 
+```
+$ node main
+```
+## Go to
+http:localhost:2017 voalá :+1:
---
+++ public/js/iniciar_sesion.js
@@ -0,0 +1,25 @@
+var app = angular.module('myApp', []);
+ app.controller('myCtrl', function($scope,$http) {
+ $scope.data = {};
+ 
+$scope.submit= function(){
+   
+document.getElementById('loader').style.visibility = 'visible';         // Show
+    $http({
+        url: 'http://localhost:2017/api/usuario/'+$scope.data.email,
+        method: 'POST',
+        data: {'clave':$scope.data.clave}
+     }).then(function successCallback(response) {
+               document.getElementById('loader').style.visibility = 'hidden';           // Hide
+      location.href = '/pages/concurso_admin.html#/concurso/'+response.data.usuario[0]._id; 
+      // location.href = 'concurso_admin.html'; 
+  }, function errorCallback(response) {
+        document.getElementById('loader').style.visibility = 'hidden';           // Hide
+    document.getElementById("mensaje_crear_concurso").innerHTML = response.data.mensaje;
+    
+                 $("#myModal").modal();
+    
+  });
+    
+   }
+ });
