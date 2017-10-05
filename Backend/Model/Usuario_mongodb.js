41791e4c410b38fc73ac55f16e7ea51875b208cd
makito <makitodev@outlook.com>
2017-10-05 12:02:45 -0500

    Se reestablece el modelo de usuario para funcionar con mongoDB
    Se eliminan todas las referencias de modelo para mysql y se re
    establecen todas las propiedades e imports para mongodb

--- Backend/Model/Usuario_mongodb.js
+++ Backend/Model/Usuario_mongodb.js
@@ -1,7 +1,7 @@
 'user strict'
 
-// const mongoose  = require('mongoose')
-// const Schema = mongoose.Schema
+const mongoose  = require('mongoose')
+const Schema = mongoose.Schema
 //modulo de nodejs para enccriptar el password
 const bcrypt = require('bcrypt-nodejs')
 //modulo de node con funciones de criptografia
@@ -9,94 +9,34 @@ const crypto = require('crypto')
 //Se obtiene el puerto desde una variable de entorno o se seta en 3000 si no se indica nda
 var config = require('../config')
 
-//db
-const mysql = require('mysql2')
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize(config.MySql_db, config.MySql_user, config.Mysql_pass, 
-                  {
-                    host: config.MySql_host,
-                    dialect: 'mysql',
-                    port: config.MySql_port
-                  });
-
-const UsuarioSchema = sequelize.define('Usuario', {
-    _id:{ type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
-    nombres: {type: Sequelize.STRING},
-    apellidos: {type: Sequelize.STRING},
-    email: {type: Sequelize.STRING, unique: true, lowercase: true},
-    clave: {type: Sequelize.STRING /*select:false*/}, //para que los get no retornen el password
-    fechaRegistro: {type: Sequelize.DATE, defaultValue: Sequelize.NOW},
-    fechaUltimoIngreso: {type: Sequelize.DATE},
-    perfil: {type: Sequelize.STRING, enum: ['Admin', 'Concursante'], defaultValue: 'Admin'}
-  },
-    { 
-        hooks: {
-            beforeCreate: (user) => {
-                bcrypt.genSalt(10, (err, salt) => {
-                    if(err)
-                        return next(err)
-                    bcrypt.hash(user.clave, salt, null, (err, hash) => {
-                        if(err)
-                            return next(err)
-                        user.clave = hash
-                        
-                    })
-                })
-            }
-        }
-        
-    });
-
-    UsuarioSchema.sync({logging: console.log})
-
-
-//Funciòn que se ejecuta antes de que el 
-//modelo se almacena en la base de datos
-//y permite encriptar la contraseña.
-// UsuarioSchema.beforeCreate((user, next) => {
-//     //si el usuario no ha modificado su contraseña que continúe
-//     bcrypt.genSalt(10, (err, salt) => {
-//         if(err)
-//             return next(err)
-//         bcrypt.hash(user.clave, salt, null, (err, hash) => {
-//             if(err)
-//                 return next(err)
-//             user.clave = hash
-            
-//         })
-//     })
-//   });
-
-//Para exportar el modelo creado de mysql
-module.exports = UsuarioSchema 
-
 //Modelo en mongodb
-// const UsuarioSchema = Schema({
-    // nombres: String,
-    // apellidos: String,
-    // email: {type: String, unique: true, lowercase: true},
-    // clave: {type: String, /*select:false*/}, //para que los get no retornen el password
-    // fechaRegistro: {type: Date, default: Date.now()},
-    // fechaUltimoIngreso: Date,
-    // perfil: {type: String, enum: ['Admin', 'Concursante'], default: 'Admin'}
-// })
+const UsuarioSchema = Schema({
+    nombres: String,
+    apellidos: String,
+    email: {type: String, unique: true, lowercase: true},
+    clave: {type: String, /*select:false*/}, //para que los get no retornen el password
+    fechaRegistro: {type: Date, default: Date.now()},
+    fechaUltimoIngreso: Date,
+    perfil: {type: String, enum: ['Admin', 'Concursante'], default: 'Admin'}
+})
 
-// UsuarioSchema.pre('save'/*antes de guardar*/, function(next){
-//     let user = this
-//     //si el usuario no ha modificado su contraseña que continúe
-//     if(!user.isModified('clave')) 
-//         return next()
-//     bcrypt.genSalt(10, (err, salt) => {
-//         if(err)
-//             return next(err)
-//         bcrypt.hash(user.clave, salt, null, (err, hash) => {
-//             if(err)
-//                 return next(err)
-//             user.clave = hash
-//             next()
-//         })
-//     })
-// })
+//Método que encripta la contraseña
+UsuarioSchema.pre('save'/*antes de guardar*/, function(next){
+    let user = this
+    //si el usuario no ha modificado su contraseña que continúe
+    if(!user.isModified('clave')) 
+        return next()
+    bcrypt.genSalt(10, (err, salt) => {
+        if(err)
+            return next(err)
+        bcrypt.hash(user.clave, salt, null, (err, hash) => {
+            if(err)
+                return next(err)
+            user.clave = hash
+            next()
+        })
+    })
+})
 
 //Función para asignar un avatar a través del mail
 //Hace uso de usa librería crypto para 
@@ -111,4 +51,4 @@ module.exports = UsuarioSchema
 
 
 //Para exportar el modelo a mongo, se le da un nombre y el esquema asociado
-//module.exports = mongoose.model('Usuario', UsuarioSchema)
+module.exports = mongoose.model('Usuario', UsuarioSchema)
