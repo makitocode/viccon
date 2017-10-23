6bfac34bdd875eb9b2639e758d5f402ac5514d5c
Taidy Marrugo <tj.marrugo10@uniandes.edu.co>
2017-10-22 23:07:10 -0500

    Modificacion cron converter con add SQS

--- Backend/cron/cron-converter.js
+++ Backend/cron/cron-converter.js
@@ -5,7 +5,7 @@ var converterVideo =require('./converter.js');// module del grupo 4 para el conv
 var constants = require("../config.js");
 
 var task =cron.schedule('* * * * *', function(){//se ejecuta cada minuto
-//var task =cron.schedule('0 0 */1 * * *', function(){//se ejecuta cada 2  horas	
+//var task =cron.schedule('0 0 */1 * * *', function(){//se ejecuta cada 2  horas  
 console.log("inicia llamado cron");
 //var url=constants.pathREST+"video/estado/SinProcesar";
 var url = "/consultarmensaje";
@@ -23,27 +23,33 @@ request.get({
     } else {
       //se obtiene el id del video 
       var _idVideo = data.idVideo; 
+      console.log("id video "+_idVideo);
       //Se obtiene el id del mensaje 
       var _idMensajeCola = data.idmensaje;
       
       //Se llama el api rest para obtener los datos del video
-      ConsultaVideo(_idVideo,function (respuestaConversion) {           
+      ConsultaVideo(_idVideo,function (respuestaConsulta) {           
+        console.log("respuesta "+respuestaConsulta.nombreVideo);
         //lo que responda el sevicio
         //llamo el converter.js y le envio los parametros de video original, video convertido, correo y _id
-        * * * * respuestaConsultagetvideo
-      	var p = converterVideo.converter;
-        var videoInput = respuestaConsultagetvideo.nombreVideo+'.'+respuestaConsultagetvideo.nombreExtensionVideoOriginal;
-        var videoOutput = respuestaConsultagetvideo.nombreVideo;
-        p(videoInput,videoOutput,respuestaConsultagetvideo.email,respuestaConsultagetvideo._id,function (respuestaConversion) {
+        //* * * * respuestaConsultagetvideo
+  /*      var p = converterVideo.converter;
+        var videoInput = respuestaConsulta.nombreVideo+'.'+respuestaConsulta.nombreExtensionVideoOriginal;
+        var videoOutput = respuestaConsulta.nombreVideo;
+        p(videoInput,videoOutput,respuestaConsulta.email,_idVideo,function (respuestaConversion) {
+            console.log("resp video: "+respuestaConversion);
+            //saco mensaje de la cola
+*/
+        /*
         if(respuestaConversion=='200'){//convierte ok todo y envia notificacion
             console.log("resp video: "+respuestaConversion);
         }else{
           console.log(respuestaConversion);
-        }
-        });
+        }*/
+//        });
 
       });
-      	
+        
 
     }
 });
@@ -65,7 +71,7 @@ function ConsultaVideo(video,callback){
         console.log('Error parsing JSONGET!');
     } else {//traigo el video con el id
         //lo que vas a hacer
-        //------> Retornar nombreVideo, nombreExtensionVideoOriginal, email, _id
+        callback(data.video);
       }
     }
   );
