5b4ef99a9068c59d2703d1a6115904ba2bd68cc7
Emilio Alvarado <emilio.alvarado@gmail.com>
2017-11-04 15:39:47 -0500

    cambio en ruta de acceso a api
    cambio en ruta de acceso a api

--- Backend/config.js
+++ Backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 2017,
+    port: process.env.PORT || 80,
     db: process.env.MONGODB || 'mongodb://18.216.188.216:27017/viccon',
     MySql_db: 'Viccon',
     MySql_user: 'root',
@@ -11,8 +11,8 @@ module.exports = {
     nombreCarpetaConvertida: 'conversion/',
     nombreCarpetaThumb: 'thumb/',
     extension:'.mp4',
-    pathREST:'http://localhost:2017/api/',
-    pathAPPjs:'http://localhost:2017',
+    pathREST:'https://viccon-r4.herokuapp.com/api/',
+    pathAPPjs:'https://viccon-r4.herokuapp.com',
     nombreBucket:'s3.viccon.bucket1',
     rutaMultimediaCron: '/home/ec2-user/viccon/public/media/',
     QueueUrl: 'https://sqs.us-east-2.amazonaws.com/911317221798/sqs-viccon-video-standard',
